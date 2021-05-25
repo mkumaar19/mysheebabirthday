56c68628c771d073cfade4103d05d844ed22cd7a
mkumaar19 <68144113+mkumaar19@users.noreply.github.com>
2021-05-26 00:56:24 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -8,11 +8,11 @@ $('document').ready(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6').stop();
 			$('#b11').animate({top:220, left: 0},500);
-			$('#b22').animate({top:240, left: 15},500);
-			$('#b33').animate({top:240, left: 30},500);
-			$('#b44').animate({top:240, left: 45},500);
-			$('#b55').animate({top:240, left: 60},500);
-			$('#b66').animate({top:240, left: 75},500);
+			$('#b22').animate({top:240, left: '15%'},500);
+			$('#b33').animate({top:240, left: '30%'},500);
+			$('#b44').animate({top:240, left: '45%'},500);
+			$('#b55').animate({top:240, left: '60%'},500);
+			$('#b66').animate({top:240, left: '75%'},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -150,11 +150,11 @@ $('document').ready(function(){
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
 		$('#b11').animate({top:230, left: 0},500);
-		$('#b22').animate({top:240, left: 15},500);
-		$('#b33').animate({top:240, left: 30},500);
-		$('#b44').animate({top:240, left: 45},500);
-		$('#b55').animate({top:240, left: 60},500);
-		$('#b66').animate({top:240, left: 75},500);
+		$('#b22').animate({top:240, left: '15%'},500);
+		$('#b33').animate({top:240, left: '30%'},500);
+		$('#b44').animate({top:240, left: '45%'},500);
+		$('#b55').animate({top:240, left: '60%'},500);
+		$('#b66').animate({top:240, left: '75%'},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
